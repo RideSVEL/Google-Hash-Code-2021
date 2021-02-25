a1c21fe5f3adcd80c5c0f1ab5240ab8ddf871c1d
Sergey Vasilchenko <sbkiking3@gmail.com>
2021-02-25 21:07:11 +0200

    Add input reader

--- src/main/java/InputData.java
+++ src/main/java/InputData.java
@@ -1,29 +1,30 @@
+import lombok.Data;
+
 import java.util.List;
 
+@Data
 public class InputData {
-    private final long durationD;
-    private final long intersectionsI;
-    private final long streetsS;
-    private final long carsV;
-    private final int bonusF;
+
+    private int durationD;
+    private int intersectionsI;
+    private int streetsS;
+    private int carsV;
+    private int bonusF;
+    private List<String> streetsLines;
+    private List<String> waysLines;
 
     public InputData(List<String> lines) {
-        String[] s = lines.get(0).split(" ");
-        this.durationD = Long.parseLong(s[0]);
-        this.intersectionsI = Long.parseLong(s[1]);
-        this.streetsS = Long.parseLong(s[2]);
-        this.carsV = Long.parseLong(s[3]);
+        parseFirstLine(lines.get(0).split(" "));
+        this.streetsLines = lines.subList(1, streetsS - 1);
+        this.waysLines = lines.subList(carsV, lines.size() - 1);
+    }
+
+    private void parseFirstLine(String[] s) {
+        this.durationD = Integer.parseInt(s[0]);
+        this.intersectionsI = Integer.parseInt(s[1]);
+        this.streetsS = Integer.parseInt(s[2]);
+        this.carsV = Integer.parseInt(s[3]);
         this.bonusF = Integer.parseInt(s[4]);
     }
 
-    @Override
-    public String toString() {
-        return "InputData{" +
-                "durationD=" + durationD +
-                ", intersectionsI=" + intersectionsI +
-                ", streetsS=" + streetsS +
-                ", carsV=" + carsV +
-                ", bonusF=" + bonusF +
-                '}';
-    }
 }
