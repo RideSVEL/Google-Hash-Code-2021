09bba324103bf84e705b7502145942c751f30cc1
Sergey Vasilchenko <sbkiking3@gmail.com>
2021-02-25 22:12:30 +0200

    Initialize cars

--- src/main/java/InputData.java
+++ src/main/java/InputData.java
@@ -1,9 +1,8 @@
+import com.hashcode.models.Car;
 import com.hashcode.models.Street;
 import lombok.Data;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Data
 public class InputData {
@@ -14,14 +13,14 @@ public class InputData {
     private int carsV;
     private int bonusF;
     private List<String> waysLines;
-    private Map<String, Street> streetMap;
+    private Map<String, Street> streetMap = new HashMap<>();
+    private List<Car> cars = new ArrayList<>();
 
 
     public InputData(List<String> lines) {
         parseFirstLine(lines.get(0).split(" "));
-        this.waysLines = lines.subList(streetsSCounts + 1, lines.size());
-        this.streetMap = new HashMap<>();
         initializeStreets(lines.subList(1, streetsSCounts + 1));
+        initializeCars(lines.subList(streetsSCounts + 1, lines.size()));
     }
 
     private void parseFirstLine(String[] s) {
@@ -37,6 +36,17 @@ public class InputData {
             String[] s1 = s.split(" ");
             Street street = new Street(s1[2], Integer.parseInt(s1[3]), Integer.parseInt(s1[0]), Integer.parseInt(s1[1]));
             streetMap.put(street.getName(), street);
+        }
+    }
+
+    private void initializeCars(List<String> lines) {
+        for (String s : lines) {
+            String[] s1 = s.split(" ");
+            List<Street> streets = new ArrayList<>();
+            for (int i = 1; i < s1.length; i++) {
+                streets.add(streetMap.get(s1[i]));
+            }
+            cars.add(new Car(Integer.parseInt(s1[0]), streets));
         }
     }
 
